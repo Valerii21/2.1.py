5ab00151bb5cd9741b82e82600a3d8d6ca2f258e
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-09-28 12:34:35 +0300

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,28 @@
+""" Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+Пример:
+- 6782 -> 23
+- 0,56 -> 11 """
+
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = float(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def sumNums(num):
+    sum = 0
+    for i in str(num):
+        if i != ".":
+            sum += int(i)
+    return sum
+
+
+num = InputNumbers("Введите число: ")
+
+print(f"Сумма цифр = {sumNums(num)}")
